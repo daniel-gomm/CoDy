7f6a781402891adde990e1fc48f00c21c6dfa15d
daniel-gomm <daniel.gomm@outlook.de>
2023-09-15 14:02:46 +0200

    Introduce data preprocessing capabilities

---
+++ CFTGNNExplainer/constants.py
@@ -0,0 +1,11 @@
+COL_NODE_I = 'item_id'
+COL_NODE_U = 'user_id'
+COL_TIMESTAMP = 'timestamp'
+COL_STATE = 'state_label'
+COL_ID = 'idx'
+
+PREFIX_EDGE_FEATURE_COL = 'f_'
+
+TRAIN_START_MEMORY_LABEL = 'train-start'
+TRAIN_END_MEMORY_LABEL = 'train-end'
+VALIDATION_END_MEMORY_LABEL = 'validation-end'
