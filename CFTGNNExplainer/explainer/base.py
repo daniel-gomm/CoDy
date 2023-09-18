fb0139011de2a672634b09b745e311eb13f76901
daniel-gomm <daniel.gomm@outlook.de>
2023-09-18 08:53:15 +0200

    Simplify and generify greedy cf explainer implementation

--- CFTGNNExplainer/explainer/base.py
+++ CFTGNNExplainer/explainer/base.py
@@ -1,8 +1,14 @@
 import logging
+from typing import List
+
 import numpy as np
+import pandas as pd
 from dataclasses import dataclass
 
 from CFTGNNExplainer.connector.bridge import TGNNBridge
+from CFTGNNExplainer.constants import EXPLAINED_EVENT_MEMORY_LABEL, CURRENT_ITERATION_MIN_EVENT_MEMORY_LABEL
+from CFTGNNExplainer.data.subgraph import SubgraphGenerator
+from CFTGNNExplainer.explainer.sampler import EdgeSampler, RandomEdgeSampler, RecentEdgeSampler, ClosestEdgeSampler
 
 
 @dataclass
@@ -19,19 +25,61 @@ class CounterFactualExample:
                 f'event importances {str(self.get_relative_importances().tolist())}')
 
     def get_absolute_importances(self) -> np.ndarray:
-        return np.array([importance - np.sum(self.event_importances[index-1:index]) for index, importance in
+        return np.array([importance - np.sum(self.event_importances[index - 1:index]) for index, importance in
                          enumerate(self.event_importances)])
 
     def get_relative_importances(self) -> np.ndarray:
         return self.get_absolute_importances() / self.event_importances[-1]
 
 
+def calculate_prediction_delta(original_prediction: float, prediction_to_assess: float):
+    if prediction_to_assess * original_prediction < 0:
+        return abs(prediction_to_assess) + abs(original_prediction)
+    return abs(original_prediction) - abs(prediction_to_assess)
+
+
 class Explainer:
 
-    def __init__(self, tgnn_bridge: TGNNBridge):
+    def __init__(self, tgnn_bridge: TGNNBridge, sampling_strategy: str = 'recent'):
         self.tgnn_bridge = tgnn_bridge
+        self.dataset = self.tgnn_bridge.model.dataset
+        self.subgraph_generator = SubgraphGenerator(self.dataset)
+        self.num_hops = self.tgnn_bridge.model.num_hops
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger()
+        self.sampling_strategy = sampling_strategy
 
-    def explain(self, explained_event_id: int) -> CounterFactualExample:
+    def _create_sampler(self, subgraph: pd.DataFrame) -> EdgeSampler:
+        if self.sampling_strategy == 'random':
+            return RandomEdgeSampler(subgraph)
+        elif self.sampling_strategy == 'recent':
+            return RecentEdgeSampler(subgraph)
+        elif self.sampling_strategy == 'closest':
+            return ClosestEdgeSampler(subgraph)
+        else:
+            raise NotImplementedError(f'No sampler implemented for sampling strategy {self.sampling_strategy}')
+
+    def calculate_original_score(self, explained_event_id: int, min_event_id: int, verbose: bool = False):
+        self.tgnn_bridge.initialize(min_event_id, show_progress=verbose, memory_label=EXPLAINED_EVENT_MEMORY_LABEL)
+        self.tgnn_bridge.initialize(explained_event_id - 1)
+        original_prediction, _ = self.tgnn_bridge.predict(explained_event_id, result_as_logit=True)
+        original_prediction = original_prediction.detach().cpu().item()
+        if verbose:
+            self.logger.info(f'Original prediction {original_prediction}')
+        return original_prediction
+
+    def calculate_subgraph_prediction(self, candidate_events: np.ndarray,
+                                      subgraph: np.ndarray, cf_example_events: List[int], explained_event_id: int,
+                                      candidate_event_id: int):
+        self.tgnn_bridge.initialize(np.min(candidate_events) - 1, show_progress=False,
+                                    memory_label=CURRENT_ITERATION_MIN_EVENT_MEMORY_LABEL)
+        subgraph_prediction, _ = self.tgnn_bridge.predict_from_subgraph(explained_event_id,
+                                                                        subgraph,
+                                                                        np.array(cf_example_events +
+                                                                                 [candidate_event_id]),
+                                                                        result_as_logit=True)
+        subgraph_prediction = subgraph_prediction.detach().cpu().item()
+        return subgraph_prediction
+
+    def explain(self, explained_event_id: int, verbose: bool = False) -> CounterFactualExample:
         raise NotImplementedError
--- CFTGNNExplainer/explainer/greedy.py
+++ CFTGNNExplainer/explainer/greedy.py
@@ -1,17 +1,11 @@
 import numpy as np
-import pandas as pd
 from CFTGNNExplainer.connector.bridge import TGNNBridge
 from CFTGNNExplainer.constants import COL_ID, EXPLAINED_EVENT_MEMORY_LABEL, CURRENT_ITERATION_MIN_EVENT_MEMORY_LABEL
-from CFTGNNExplainer.data.subgraph import SubgraphGenerator
-from CFTGNNExplainer.explainer.base import Explainer, CounterFactualExample
-from CFTGNNExplainer.explainer.sampler import EdgeSampler, RandomEdgeSampler, RecentEdgeSampler, ClosestEdgeSampler
+from CFTGNNExplainer.explainer.base import Explainer, CounterFactualExample, calculate_prediction_delta
 
 
 def is_prediction_most_shifted(original_prediction: float, prediction_to_assess: float, previous_delta: float):
-    if prediction_to_assess * original_prediction < 0:
-        delta = abs(prediction_to_assess) + abs(original_prediction)
-    else:
-        delta = abs(original_prediction) - abs(prediction_to_assess)
+    delta = calculate_prediction_delta(original_prediction, prediction_to_assess)
     if previous_delta < delta:
         return True, delta
     return False, delta
@@ -21,23 +15,9 @@ class GreedyCFExplainer(Explainer):
 
     def __init__(self, tgnn_bridge: TGNNBridge, candidates_size: int = 75, sample_size: int = 10,
                  sampling_strategy: str = 'recent'):
-        super().__init__(tgnn_bridge)
-        self.dataset = self.tgnn_bridge.model.dataset
-        self.subgraph_generator = SubgraphGenerator(self.dataset)
-        self.num_hops = self.tgnn_bridge.model.num_hops
+        super().__init__(tgnn_bridge, sampling_strategy)
         self.candidates_size = candidates_size
         self.sample_size = sample_size
-        self.sampling_strategy = sampling_strategy
-
-    def _create_sampler(self, subgraph: pd.DataFrame) -> EdgeSampler:
-        if self.sampling_strategy == 'random':
-            return RandomEdgeSampler(subgraph)
-        elif self.sampling_strategy == 'recent':
-            return RecentEdgeSampler(subgraph)
-        elif self.sampling_strategy == 'closest':
-            return ClosestEdgeSampler(subgraph)
-        else:
-            raise NotImplementedError(f'No sampler implemented for sampling strategy {self.sampling_strategy}')
 
     def explain(self, explained_event_id: int, verbose: bool = False) -> CounterFactualExample:
         subgraph = self.subgraph_generator.get_fixed_size_k_hop_temporal_subgraph(num_hops=self.num_hops,
@@ -48,12 +28,8 @@ class GreedyCFExplainer(Explainer):
 
         self.tgnn_bridge.set_evaluation_mode(True)
         self.tgnn_bridge.reset_model()
-        self.tgnn_bridge.initialize(min_event_id, show_progress=verbose, memory_label=EXPLAINED_EVENT_MEMORY_LABEL)
-        self.tgnn_bridge.initialize(explained_event_id - 1)
-        original_prediction, _ = self.tgnn_bridge.predict(explained_event_id, result_as_logit=True)
-        original_prediction = original_prediction.detach().cpu().item()
-        if verbose:
-            self.logger.info(f'Original prediction {original_prediction}')
+
+        original_prediction = self.calculate_original_score(explained_event_id, min_event_id, verbose)
 
         remaining_subgraph = subgraph[COL_ID].to_numpy()
         remaining_subgraph = remaining_subgraph[remaining_subgraph != explained_event_id]
@@ -74,14 +50,10 @@ class GreedyCFExplainer(Explainer):
                                         memory_label=EXPLAINED_EVENT_MEMORY_LABEL)
             for candidate_event_id in candidate_events:
                 current_subgraph = remaining_subgraph[remaining_subgraph != candidate_event_id]
-                self.tgnn_bridge.initialize(np.min(candidate_events) - 1, show_progress=False,
-                                            memory_label=CURRENT_ITERATION_MIN_EVENT_MEMORY_LABEL)
-                current_subgraph_prediction, _ = self.tgnn_bridge.predict_from_subgraph(explained_event_id,
-                                                                                        current_subgraph,
-                                                                                        np.array(cf_example_events +
-                                                                                                 [candidate_event_id]),
-                                                                                        result_as_logit=True)
-                current_subgraph_prediction = current_subgraph_prediction.detach().cpu().item()
+
+                current_subgraph_prediction = self.calculate_subgraph_prediction(candidate_events, current_subgraph,
+                                                                                 cf_example_events, explained_event_id,
+                                                                                 candidate_event_id)
                 is_most_shifted, delta = is_prediction_most_shifted(original_prediction, current_subgraph_prediction,
                                                                     largest_prediction_delta)
                 if verbose:
