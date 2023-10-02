fa0442713ac7f45a3468aa37dff252c9a877823a
daniel-gomm <daniel.gomm@outlook.de>
2023-10-02 21:17:18 +0200

    [enhancement] Update setup.py to install all required dependencies when installing the package

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+
 from setuptools import setup, find_packages
 
 setup(
@@ -6,11 +8,16 @@ setup(
     description='Counterfactual explanations for graph neural networks on dynamic graphs',
     author='Daniel Gomm',
     author_email='daniel.gomm@student.kit.edu',
+    url='https://github.com/daniel-gomm/CFTGNNExplainer',
+    keywords='explanation, gnn, tgnn, counterfactual',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'pandas',
-        'torch',
-        'tqdm'
-    ],
+        'numpy==1.25.2',
+        'pandas==2.0.1',
+        'torch==2.0.1',
+        'tqdm==4.65.0',
+        'ipython',
+        f"TGN @ file://localhost/{os.getcwd()}/submodules/tgn/",
+        f"TTGN @ file://localhost/{os.getcwd()}/submodules/ttgn/"
+    ]
 )
