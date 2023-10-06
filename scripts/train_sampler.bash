1dc4d60b7a3d8e2752bd1ff9bec3627e70d4a7e6
daniel-gomm <daniel.gomm@outlook.de>
2023-10-06 16:33:04 +0200

    [feature] Add script for starting the sampler training

---
+++ scripts/train_sampler.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source "$SCRIPT_DIR/common.bash"
+
+train_sampler() {
+  TGN_PATH="$PARENT_DIR/resources/models/$1/$1-$1.pth"
+  MODEL_PATH="$PARENT_DIR/resources/models/$1/sampler"
+  if [ ! -d "$MODEL_PATH" ]; then
+    mkdir -p "$MODEL_PATH"
+    echo "Created new directory for the final Sampler model and model checkpoints for the $1 dataset at $MODEL_PATH"
+  fi
+  echo "Training Sampler model for the $1 dataset..."
+  python "$SCRIPT_DIR/train_sampler.py" -d "$PROCESSED_DATA_DIR/$1" --bipartite --cuda --dynamic --model_save_path "$MODEL_PATH" --train_examples 250 --depth 2 --sample_size 25 --epochs 500 --model $TGN_PATH --candidates_size 75
+}
+
+
+show_help() {
+  echo -e "
+Train Sampler script
+
+Usage: bash $SCRIPT_DIR/train_sampler.bash ${RED}DATASET-NAME${NC}
+
+For the ${RED}DATASET-NAME${NC} parameter provide the name of any of the preprocessed datasets.
+Possible values: ${CYAN}[${DATASET_NAMES[*]}]${NC}
+"
+exit 1
+}
+
+if [ $# -eq 0 ]; then
+  show_help
+else
+  test_exists "$1" "${DATASET_NAMES[@]}"
+  train_sampler "$1"
+fi
--- scripts/train_sampler.py
+++ scripts/train_sampler.py
@@ -280,18 +280,14 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=5, help='Number of epochs to train the model for.')
     parser.add_argument('--model_save_path', required=True, type=str,
                         help='Path at which to save the model and its checkpoints')
-    parser.add_argument('--resume_path', required=False, default=None, type=str,
-                        help='Path of a pretrained sampler model on which to resume training.')
     parser.add_argument('--train_examples', type=int, default=100, required=True,
                         help='Number of explained events to use to train the sampler')
     parser.add_argument('--depth', type=int, default=2, required=True,
                         help='Number of sampling steps per explained event')
     parser.add_argument('--sample_size', type=int, default=10,
                         help='Number of samples to draw in each sampling step')
-    parser.add_argument('--candidates_size', type=int, default=25,
+    parser.add_argument('--candidates_size', type=int, default=75,
                         help='Number of candidates from which the samples are selected')
-    parser.add_argument('--retraining_epochs', type=int, default=0,
-                        help='Number of epochs for training the prediction model on the data from the initial training')
 
     args = parse_args(parser)
 
